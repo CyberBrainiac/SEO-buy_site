eb7b5a43b253fe6faab0f164b4b693cb3d143962
Arseniy Arkhipov <arsenij.arxipov.1998@gmail.com>
2023-09-29 18:54:42 +0300

    create drag and drop

--- src/pages/thematIndex/ThematicityIndex.tsx
+++ src/pages/thematIndex/ThematicityIndex.tsx
@@ -1,10 +1,15 @@
 import { ButtonLink } from '@/components/buttons/Buttons';
+import style from './thematicityIndex.module.scss';
+import InputFile from './inputFile/InputFile';
 
 const ThematicityIndex: React.FC = () => {
+  const handleFileUpload = (file: File) => {
+    console.log('Uploaded file:', file);
+  };
+
   return (
-    <section>
-      INDEX THEMATICITY PAGE
-      <div>Content</div>
+    <section className={style.container}>
+      <InputFile onFileUpload={handleFileUpload} />
       <ButtonLink href="/" text="Home page" />
     </section>
   );
---
+++ src/pages/thematIndex/inputFile/InputFile.tsx
@@ -0,0 +1,40 @@
+import React, { useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
+
+interface InputFileProps {
+  onFileUpload: (file: File) => void;
+}
+
+const InputFile: React.FC<InputFileProps> = ({ onFileUpload }) => {
+  const onDrop = useCallback(
+    (acceptedFiles: File[]) => {
+      // Do something with the uploaded file
+      const uploadedFile = acceptedFiles[0];
+      onFileUpload(uploadedFile);
+    },
+    [onFileUpload]
+  );
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div {...getRootProps()} style={dropzoneStyles}>
+      <input {...getInputProps()} />
+      <p>
+        {isDragActive
+          ? 'Drop the file here'
+          : 'Drag and drop a file here, or click to select a file'}
+      </p>
+    </div>
+  );
+};
+
+const dropzoneStyles: React.CSSProperties = {
+  border: '2px dashed #ccc',
+  borderRadius: '4px',
+  padding: '20px',
+  textAlign: 'center',
+  cursor: 'pointer',
+};
+
+export default InputFile;
