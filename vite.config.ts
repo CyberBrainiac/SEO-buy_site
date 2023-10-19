efbce2ee108780bfa6aa4293770290d296885fe0
Arseniy <arsenij.arxipov.1998@gmail.com>
2023-10-19 09:09:37 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,6 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/site_indexThematicity/',
-  server: {
-    port: 4200,
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src/'),
